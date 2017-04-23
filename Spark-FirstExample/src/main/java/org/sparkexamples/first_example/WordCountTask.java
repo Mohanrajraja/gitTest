162383c621416e2378a5613a15467ae24f3a520f
Mohanraj <mohanraj.raja15@gmail.com>
2017-04-23 10:38:53 +0530

    spark simple program working

--- Spark-FirstExample/src/main/java/org/sparkexamples/first_example/WordCountTask.java
+++ Spark-FirstExample/src/main/java/org/sparkexamples/first_example/WordCountTask.java
@@ -5,7 +5,9 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
+import scala.reflect.io.Path;
 
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -26,14 +28,20 @@ public class WordCountTask {
    * See {@see http://spark.apache.org/docs/latest/submitting-applications.html}
    */
   public static void main(String[] args) {
-    checkArgument(args.length > 1, "Please provide the path of input file as first parameter.");
-    new WordCountTask().run(args[1]);
+	 //String path =  "E:\Studies\hdata\Spark" ;
+	 java.nio.file.Path sourceFilePath =  Paths.get("E:/Studies/hdata/Spark/WordCount.txt");
+	 System.out.println(" path   : "+ sourceFilePath.toString());
+	 System.out.println(" path   : "+ args[0].toString() + " " + args.length);
+	 
+    //checkArgument(args.length < 1, "Please provide the path of input file as first parameter.");
+    //new WordCountTask().run(sourceFilePath.toString());
+    new WordCountTask().run(args[0], args[1]);
   }
 
   /**
    * The task body
    */
-  public void run(String inputFilePath) {
+  public void run(String inputFilePath, String OutputDirectory) {
     /*
      * This is the address of the Spark cluster. We will call the task from WordCountTest and we
      * use a local standalone cluster. [*] means use all the cores available.
@@ -52,6 +60,13 @@ public class WordCountTask {
     /*
      * Performs a work count sequence of tasks and prints the output with a logger.
      */
-    context.textFile(inputFilePath);
+    context.textFile(inputFilePath).flatMap(text -> Arrays.asList(text.split(" ")).iterator())
+    .mapToPair(word -> new Tuple2<>(word, 1))
+    .reduceByKey((a, b) -> a + b).saveAsTextFile(OutputDirectory);
+    
+    
+    //.foreach(result -> LOGGER.info(
+    //    String.format("Word [%s] count [%d].", result._1(), result._2)));
+    
   }
 }
